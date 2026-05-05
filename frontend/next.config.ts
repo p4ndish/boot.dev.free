a371f806f6c11b45d6841bd7aa406e5cb67c0797
p4ndish <p4ndish.dev@gmail.com>
2026-05-05 03:49:31 +0200

    Allow remote dev access via allowedDevOrigins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["38.242.155.107", "localhost"],
 };
 
 export default nextConfig;
